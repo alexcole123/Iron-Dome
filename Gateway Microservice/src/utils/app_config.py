d73ba2b78a9940fc2639455008acb863a2c05c77
alexj.cole@hotmail.com <alexj.cole@hotmail.com>
2024-06-08 16:40:04 +0300

    initial commit

---
+++ Gateway Microservice/src/utils/app_config.py
@@ -0,0 +1,5 @@
+class AppConfig:
+    attacks_url = "http://localhost:8001/api/attacks"
+    decisions_url = "http://localhost:8002/api/decisions"
+    intercept_url = "http://localhost:8002/api/intercept"
+
